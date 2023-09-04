e0531c200cd83f9152f3b135ce7063e350d18489
Nishant Rai <rnishant9970@gmail.com>
2023-09-04 09:52:43 +0530

    The UI is ready

---
+++ src/components/Operations.jsx
@@ -0,0 +1,17 @@
+import './Operations.css'
+
+function Operations(){
+
+    return (
+    <div className="operations">
+        
+        <div  className="expression" >6000/2+3227*2</div>
+
+        <div className="result" >=12,454</div>
+         
+    </div>
+    
+    );
+}
+
+export default Operations;
